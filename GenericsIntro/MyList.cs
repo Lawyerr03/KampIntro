c628b91b9cbb1778fa2262f5b4c901f1854af0a9
M. MUSTAFA ÇELİK <M. MUSTAFA ELK@DESKTOP-80KA9BE>
2023-02-25 15:02:19 +0300

    23 Şubat Ders SonuAdd project files.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        public void Add(T  item)
+        {
+            
+        
+        
+        }
+    }
+}
